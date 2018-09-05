60c2978bdd39adc2713b7a97011f4ba05220d065
oulianxian <oulianxian@163.com>
2018-09-05 15:52:57 +0800

    初始化项目

---
+++ src/main/java/com/kukuou/testgit/TestGitApplication.java
@@ -0,0 +1,12 @@
+package com.kukuou.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitApplication.class, args);
+	}
+}
